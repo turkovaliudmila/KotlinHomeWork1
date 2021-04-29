54763ee5213f02a6b84466d9c2fb761f8506abb8
turkovaliudmila <ljudmila327@rambler.ru>
2021-04-29 20:35:07 +0300

    В MainActivity.kt добавлен слушатель кнопки button1

--- app/src/main/java/ru/geekbrains/kotlinhomework1/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/kotlinhomework1/MainActivity.kt
@@ -1,11 +1,20 @@
 package ru.geekbrains.kotlinhomework1
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button1 = findViewById<Button>(R.id.button1)
+        button1?.setOnClickListener() {
+
+        }
     }
 }
